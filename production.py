8d43763700f34e7e37e1c28a18ccc995fabf816d
HAERANG <abcrang@gmail.com>
2022-05-04 02:57:36 +0900

    last_6

--- production.py
+++ production.py
@@ -2,6 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ekdldksk@localhost:3306/testdb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
