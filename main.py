346b6b39b5a77ac9672ecb5ac3d10e134b53fbef
HAERANG <abcrang@gmail.com>
2022-05-04 01:11:37 +0900

    lastttt

--- main.py
+++ main.py
@@ -13,15 +13,20 @@ import config
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
-migrate = Migrate()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ekdldksk@localhost:3306/testdb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-db.init_app(app)
-migrate.init_app(app, db)
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'super secret key'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ekdldksk@localhost:3306/testdb'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    db.init_app(app)
+    migrate.init_app(app, db)
 
 
 @app.route('/')
